b4ea21b3cfdae66fe642d80f230193ae0a736276
ilix-studio <ilishjyoti17@gmail.com>
2025-01-01 00:35:43 +0530

    initialSetup_ios

---
+++ src/navigation/AppNavigation.tsx
@@ -0,0 +1,14 @@
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+export default function AppNavigation() {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>AppNavigation</Text>
+      </View>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({});
